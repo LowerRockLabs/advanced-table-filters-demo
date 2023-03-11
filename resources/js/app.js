a0e89f565d255f519cab3faaa7996663e53eb669
lrljoe <joe@lowerrocklabs.com>
2023-03-11 21:35:00 +0000

    Add Alpine & Flatpickr to Demo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus';
+
+import flatpickr from "flatpickr";
+
+window.Alpine = Alpine;
+window.Alpine.plugin(focus);
+window.Alpine.start();
