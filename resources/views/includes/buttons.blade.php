4c049b2c84157863b49f2c89ea99e7524f9a20c8
lrljoe <joe@lowerrocklabs.com>
2023-03-13 02:39:47 +0000

    Change Buttons

--- resources/views/includes/buttons.blade.php
+++ resources/views/includes/buttons.blade.php
@@ -1,6 +1,6 @@
-<div>
-    <div style="text-align:center;margin-top:10px;margin-bottom:10px;">
-        <h3><strong>Pop-Over</strong></h3>
+<div style="width: 100%; text-align: center;">
+    <div style="width:25%; display:inline-block; text-align:center;margin-top:10px;margin-bottom:10px;">
+        <h3><strong>Pop-Over Filters</strong></h3>
         <div style="text-align:center;margin-top:10px;margin-bottom:10px;">
             <a href="{{ route('tw3') }}">Tailwind 3</a> |
             <a href="{{ route('tw') }}">Tailwind 2</a> |
@@ -8,13 +8,13 @@
             <a href="{{ route('bs4') }}">Bootstrap 4</a>
         </div>
     </div>
-    <div style="text-align:center;margin-top:10px;margin-bottom:10px;">
-        <h3>Slide-Down</h3>
+    <div style="width:25%; display:inline-block; text-align:center;margin-top:10px;margin-bottom:10px;">
+        <h3>Slide-Down Filters</h3>
         <div style="text-align:center;margin-top:10px;margin-bottom:10px;">
-            <a href="{{ route('tw3-slidedown') }}">Tailwind 3 SlideDown</a> |
-            <a href="{{ route('tw-slidedown') }}">Tailwind 2 SlideDown</a> |
-            <a href="{{ route('bs5-slidedown') }}">Bootstrap 5 SlideDown</a> |
-            <a href="{{ route('bs4-slidedown') }}">Bootstrap 4 SlideDown</a>
+            <a href="{{ route('tw3-slidedown') }}">Tailwind 3</a> |
+            <a href="{{ route('tw-slidedown') }}">Tailwind 2</a> |
+            <a href="{{ route('bs5-slidedown') }}">Bootstrap 5</a> |
+            <a href="{{ route('bs4-slidedown') }}">Bootstrap 4</a>
         </div>
     </div>
 </div>
